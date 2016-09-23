7ca5264b645b471a33fa3e91c3278b686b355d59
Onoyko <snpsin@gmail.com>
2016-09-23 16:01:11 +0300

    Complete code
    complete code of the project

---
+++ SeliniumFirst/Browser.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace SeliniumFirst {
+
+
+    public static class Browser {
+
+        private static string startUrl = "http://www.acumenci.com/";
+        private static IWebDriver driver = new ChromeDriver();
+
+        public static void Initialize () {
+            GoToUrl("");
+        }
+
+        public static void CleanUp() {
+
+            driver.Close();
+        }
+        public static void GoToUrl (string page) {
+
+            driver.Url = startUrl + page;
+        }
+
+        public static void ContactPage() {
+
+            GoToUrl("contact/");
+        }
+
+        public static void ClickButton(string elementType, string elementName) {
+            if(elementType == "id") {
+
+                    driver.FindElement(By.Id(elementName)).Click(); }
+
+            else if(elementType == "name") {
+                      
+                    driver.FindElement(By.Name(elementName)).Click(); }
+
+            else if(elementType == "class") {
+
+                    driver.FindElement(By.ClassName(elementName)).Click(); }
+
+            else {
+                    Console.WriteLine("error"); }
+            }
+       
+
+        public static void EnterText(string elementName, string value) {
+
+            driver.FindElement(By.Name(elementName)).SendKeys(value);
+
+            
+        }
+
+        public static bool VerifyElement(string value) {
+
+
+            if (driver.PageSource.Contains(value)) {
+
+                Console.WriteLine("Element " + value + "found");
+                return true;
+            }else
+                Console.WriteLine("Element " + value + "was not Found");
+            return false;
+        }
+
+        public static void Submit (string BUTTON_XPATH) {
+            driver.FindElement(By.XPath(BUTTON_XPATH)).Click();
+        }
+
+        public static void Alerts (string error) {
+
+            Thread.Sleep(5000);
+            var error_message = driver.FindElement(By.Id(error)).Text;
+            Console.WriteLine(error_message);
+
+
+        }
+
+
+
+            }
+
+
+    }
---
+++ SeliniumFirst/Program.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SeliniumFirst {
+    class Program {
+
+        static void Main(string[] args) {
+
+
+
+        }
+
+        [Test]
+        public void CanFindThePostCode () {
+
+            //initialize the start page
+            Browser.Initialize();
+            Browser.ClickButton("id", "menu-item-497");
+            //find the requested element on the page
+            Browser.VerifyElement("TW9 1HY");
+            //close browser
+            Browser.CleanUp();
+        }
+        [Test]
+        public void CanSeeAlerts (){
+            //go to cotnact page
+            Browser.ContactPage();
+            //enter email
+            Browser.EnterText("your-email", "xxx@xx.xx");
+            //enter text
+            Browser.EnterText("your-message", "some text");
+            //press submit
+            Browser.Submit("//*[@id='wpcf7-f682-p495-o1']/form/p[8]/input");
+            //check for alerts
+            Browser.Alerts("wpcf7-f682-p495-o1");
+            Browser.CleanUp();
+        }
+        
+    }
+
+
+}
